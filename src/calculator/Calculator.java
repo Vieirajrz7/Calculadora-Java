11b7d3db89b0c7f9791695cd1881251cd262b8b6
Marcelo Vieira Gonçalves <23012@etecdracena.local>
2024-09-06 10:49:29 -0300

    first commit

---
+++ src/calculator/Calculator.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package calculator;
+
+/**
+ *
+ * @author 23012
+ */
+public class Calculator {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // Calling Screen Calculator
+        
+        ScreenCalculator screen = new ScreenCalculator();
+        screen.show();
+        
+    }
+    
+}
